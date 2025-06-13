cc1d0d80b687ecbbf1fea240bec44a920297068f
James Morton <jamesmorton@Jamess-MBP.localdomain>
2025-06-13 14:11:28 +0100

    Finished most of the portfolio page including media queries but need to still add darkmode and other smaller features

--- script.js
+++ script.js
@@ -6,3 +6,31 @@ window.addEventListener('scroll', function() {
       topnav.classList.remove('black');
     }
   });
+
+
+function techStackFunction() {
+  var project1 = document.getElementById('port-project-1')
+  var project2 = document.getElementById('port-project-2')
+  var project3 = document.getElementById('port-project-3')
+  var project4 = document.getElementById('port-project-4')
+  var techstack = document.getElementById('port-tech-section')
+  project1.classList.add('hidden')
+  project2.classList.add('hidden')
+  project3.classList.add('hidden')
+  project4.classList.add('hidden')
+  techstack.classList.remove('hidden')
+}
+
+
+function projectFunction() {
+  var project1 = document.getElementById('port-project-1')
+  var project2 = document.getElementById('port-project-2')
+  var project3 = document.getElementById('port-project-3')
+  var project4 = document.getElementById('port-project-4')
+  var techstack = document.getElementById('port-tech-section')
+  project1.classList.remove('hidden')
+  project2.classList.remove('hidden')
+  project3.classList.remove('hidden')
+  project4.classList.remove('hidden')
+  techstack.classList.add('hidden')
+}
